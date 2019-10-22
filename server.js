fcfebcaca00d16da159774895b09b32d18cfa174
mengchaoli <lmcwesdxc@163.com>
2019-10-21 22:27:44 -0700

    add basic login/register routes
    tested using postman

--- server.js
+++ server.js
@@ -1,19 +1,21 @@
 var express    = require("express");
- var mysql      = require('mysql');
- var connection = mysql.createConnection({
-   host     : 'localhost',
-   user     : 'root',
-   password : '6112049qaZ',
-   database : 'test'
- });
- var app = express();
- 
- connection.connect(function(err){
- if(!err) {
-     console.log("Database is connected ... \n\n");  
- } else {
-     console.log("Error connecting database ... \n\n");
-     console.log(err.toString());
- }
- });
-app.listen(3000);
+var login = require('./routes/loginroutes');
+var bodyParser = require('body-parser');
+var app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+var router = express.Router();
+// test route
+router.get('/', function(req, res) {
+    res.json({ message: 'welcome to our upload module apis' });
+});
+//route to handle user registration
+router.post('/register',login.register);
+router.post('/login',login.login)
+app.use('/api', router);
+app.listen(5000);
